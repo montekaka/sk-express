81e0370f1a9f2d1a67b9b3381d941f3bc87d9c44
montekaka <josh.chen.jia@gmail.com>
2018-11-01 23:01:31 -0700

    add input selection to pick option

---
+++ client/src/components/shared/input-selection/input-selection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import _ from 'underscore';
+import { Input } from 'reactstrap';
+
+const SkInputSelect = (props) => {
+
+	function handleChange(event) {
+		props.handleSelect(event.target);
+  }
+  
+  function getOptions(options, selectedValue, keyLabel) {
+    let result = _.filter(options, (item) => {
+      return item[keyLabel] === selectedValue;
+    });
+    _.each(options, (item) => {
+      if (item[keyLabel] != selectedValue) {
+        result.push(item);
+      }
+    });
+    const listItems = result.map((item) => 
+      <option key={item[keyLabel]}>{item[keyLabel]}</option>
+    );
+    return listItems;
+  }
+
+  return (
+    <Input type="select" 
+      name={props.selectName} 
+      id="skSelect"       
+      onChange={handleChange}  >
+      {getOptions(props.options, props.selectedValue, props.optionsKeyLabel)}
+    </Input>
+  )
+};
+
+export default SkInputSelect;
