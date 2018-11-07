89811a9e763da15234119828b097c95d44505f93
montekaka <josh.chen.jia@gmail.com>
2018-11-06 23:02:18 -0800

    add a Order Class

---
+++ resource/libs/helpers/OrderClass.js
@@ -0,0 +1,41 @@
+const OrderClass = function() {
+  this.init();
+};
+
+Order.prototype.set = function(item) {
+  const keys = Object.keys(item);
+  keys.forEach((key) => {
+    if(this.form[key]) {
+      this.form[key] = item[key];
+    }    
+  });
+};
+
+Order.prototype.reset = function(item) {
+  this.init();
+};
+
+Order.prototype.init = function() {
+  this.id = -100;
+  this.order_number = 'New Order ##';
+  this.company_id = 0;
+  this.buyer_company_id = 0;
+  this.buyer_company_name = '';
+  this.buyer_id = 0;
+  this.buyer_name = '';
+  this.email = '';
+  this.phone_number = '';
+  this.total_price = 0;
+  this.total_unit = 0;
+  this.order_delivery_date = Date.now();
+  this.order_date = Date.now();
+  this.billing_address = '';
+  this.is_per_item_delivery_date = false;
+  this.is_delivered = false;
+  this.is_paid = false;
+  this.invoice_id = 0;
+  this.order_items = [];
+};
+
+
+exports.OrderClass = OrderClass;
