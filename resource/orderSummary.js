3aadb0dbf602144aaf96b0783b98d81440623617
montekaka <josh.chen.jia@gmail.com>
2018-11-15 22:22:31 -0800

    add headers to control order summary

---
+++ resource/orderSummary.js
@@ -0,0 +1,11 @@
+const skState = {
+	orderSummaryHeaders: [
+		{'id': 1,'name': "shipping_method", "format": "string", "label": "Shiping method"},
+		{'id': 2,'name': "sales_rep", "format": "string", "label": "Sales Rep"},
+		{'id': 3,'name': "terms", "format": "string", "label": "Terms"},
+		{'id': 4,'name': "slot", "format": "string", "label": "Slot"},
+		{'id': 5,'name': "total_price", "format": "money", "label": "Total"},
+	]
+}
+
+exports.skState = skState;
