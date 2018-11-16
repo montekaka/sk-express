dfdd137031aff36cad63ec1325e58cd3b7cd801e
montekaka <josh.chen.jia@gmail.com>
2018-11-15 21:41:57 -0800

    format the value of the cell

--- client/src/components/shared/simple-list/simpleListItem.jsx
+++ client/src/components/shared/simple-list/simpleListItem.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Pipe from './../../../../../resource/libs/helpers/pipe.js';
 
+const actions = Pipe.actions;
 
 const SimpleListItem = (props) => {
-	
+
+	function formatValue(item, header) {
+		const value = item['format'] ? actions[item['format']](item[header]) : item[header];
+		return value;
+	}
+ 	
 	return (
     <tr>
-      {props.headers.map((header) => <td key={header} className={props.cssClass}>{props.item[header]}</td>)}
+      {props.headers.map((header) => <td key={header} className={props.cssClass}>{formatValue(props.item, header)}</td>)}
     </tr>
   );	
 }
