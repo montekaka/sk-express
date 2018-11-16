0fc650dd6246bc6b1f13ce3d5082e36484fb5c27
montekaka <josh.chen.jia@gmail.com>
2018-11-15 23:58:42 -0800

    add redirect back to login page if its not yet sign in

--- client/src/pages/buyer_company_shipping_addresses/buyer_company_shipping_address.jsx
+++ client/src/pages/buyer_company_shipping_addresses/buyer_company_shipping_address.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Redirect } from "react-router-dom";
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import _ from 'underscore';
 import Dashheader from '../../components/dashheader/dashheader.jsx';
 import config from '../../../../resource/config';
