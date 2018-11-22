f1cc28549778ad4058058f089f55113515d9dbd5
montekaka <josh.chen.jia@gmail.com>
2018-11-21 21:37:50 -0800

    add handle enter key press to submit login

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -13,6 +13,7 @@ class Login extends React.Component {
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleEnterKeyDown = this.handleEnterKeyDown.bind(this);
 	}
 
 	handleChange(event) {
@@ -27,6 +28,14 @@ class Login extends React.Component {
 		event.preventDefault();
 	}
 
+	handleEnterKeyDown(event) {
+		if(event.keyCode === 13 && event.shiftKey === false) {
+	  	var user = this.state;
+	  	auth.signIn(user, this.props.handleUserState);			
+			event.preventDefault();
+		}
+	}
+
 	render() {
 		if (this.props.isAuthed === true) {
 			return <Redirect to='/orders' render={(props) => <Orders isAuthed={true}/>} />
@@ -35,21 +44,27 @@ class Login extends React.Component {
 			<form className="form-signin" onSubmit={this.handleSubmit}>
 				<div className="text-center mb-4">
 					<img className="mb-4" src="https://themes.getbootstrap.com/wp-content/themes/bootstrap-marketplace/assets/images/elements/bootstrap-stack.png" alt="" width="72" height="72" />
-					<h1 className="h3 mb-3 font-weight-normal">Floating labels</h1>
-					<p>Build form controls with floating labels via the <code>:placeholder-shown</code> pseudo-element. <a href="https://caniuse.com/#feat=css-placeholder-shown">Works in latest Chrome, Safari, and Firefox.</a></p>
+					<h1 className="h3 mb-3 font-weight-normal">Gobo</h1>
+					<p>Welcome to Gobo CMS</p>
 				</div>
 				<div className="form-label-group">
 	        <input type="email" name="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={this.handleChange} required />
 	        <label htmlFor="inputEmail" id="inputEmailLabel">Email address</label>
 	      </div>
-
 	      <div className="form-label-group">
-	        <input type="password" name="password" id="inputPassword" className="form-control" placeholder="Password" onChange={this.handleChange} required />
+	        <input 
+	        	type="password" 
+	        	name="password" 
+	        	id="inputPassword" 
+	        	className="form-control" 
+	        	placeholder="Password" 
+	        	onChange={this.handleChange} 
+	        	onKeyDown={this.handleEnterKeyDown}
+	        	required />
 	        <label htmlFor="inputPassword" id="inputPasswordLabel">Password</label>
 	      </div>
-
 	      <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-	      <p className="mt-5 mb-3 text-muted text-center">&copy; 2018</p>				
+	      <p className="mt-5 mb-3 text-muted text-center">&copy; 2019</p>				
 			</form>
 		)
 	}
