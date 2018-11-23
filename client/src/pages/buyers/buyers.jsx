e1bb25cecef50cad537db2b72eef86789bc0f4ea
montekaka <josh.chen.jia@gmail.com>
2018-11-22 23:46:43 -0800

    add condition rendering to handle buyers table is from buyer company or not

--- client/src/pages/buyers/buyers.jsx
+++ client/src/pages/buyers/buyers.jsx
@@ -4,6 +4,7 @@ import { Link, Redirect } from 'react-router-dom';
 import _ from 'underscore';
 import Dashheader from '../../components/dashheader/dashheader.jsx';
 import TablePagination from '../../components/shared/table-pagination/table-pagination.jsx';
+import BuyersHeader from './buyersHeader.jsx';
 import config from '../../../../resource/config';
 
 //const tableHeaders = productTable.tableHeaders;
@@ -36,23 +37,27 @@ class Buyers extends React.Component {
 	render() {
     if (this.props.isAuthed === false) {
       return <Redirect to={'/'} />
-    }  
-        
+    }
+
+    let api_base_url = this.props.base_url ? this.props.base_url : base_url;
+    let headerComponent = '';
+    if (this.props.base_url) {
+    	const new_api_base = `/new/buyer_companies/${this.props.parent_id}/buyers`
+    	headerComponent = <Link to={new_api_base} className="btn btn-sm btn-outline-info product-btn">Add buyer</Link>
+    } else {
+    	headerComponent = <BuyersHeader total={this.state.total} totalPage={this.state.totalPage}/>
+    }
 		return (
 			<div>
-				<Dashheader subtitle={'Overview'} title={'Buyers'}/>
-				<div className="hr-divider mt-3 mb-5">
-				  <h3 className="hr-divider-content hr-divider-heading">Buyers</h3>		      
-				  <p>Total Buyers: {this.state.total}</p>
-				  <p>Total Page: {this.state.totalPage}</p>
-				</div>		    
+				{ headerComponent }
 				<TablePagination 		 
 					handleUpdateTotalItems={this.handleUpdateTotalItems}   	
-					base_url={base_url}
+					base_url={api_base_url}
 					handleClickItem={this.handleClickItem} 
 					perPage={this.state.perPage}
+					parent_path={this.props.parent_path}
 					pageItemsCount={this.state.pageItemsCount}
-					skState={this.props.skState}/> 		    	
+					skState={this.props.skState}/>
 			</div>			
 		)
 	}
