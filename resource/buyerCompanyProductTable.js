ae75e2f46da56f42e56a8ff5a916ffe5f57d8717
montekaka <josh.chen.jia@gmail.com>
2018-12-01 00:33:27 -0800

    add product code, and name

--- resource/buyerCompanyProductTable.js
+++ resource/buyerCompanyProductTable.js
@@ -1,5 +1,7 @@
 const skState = {
 	tableHeaders: [
+        {'id': 1,'name': "product_code", "format": "string", "label": "Product code", "sort_by":"ASC", "sort_on": true},
+        {'id': 2,'name': "name", "format": "string", "label": "Product name", "sort_by":"ASC", "sort_on": true},	
         {'id': 3,'name': "external_code", "format": "string", "label": "External code", "sort_by":"ASC", "sort_on": true},
         {'id': 4,'name': "external_name", "format": "string", "label": "External name", "sort_by":"ASC", "sort_on": true},
         {'id': 5,'name': "external_purchase_price_category_label", "format": "string", "label": "Purchase bundle", "sort_by":"ASC", "sort_on": true},
