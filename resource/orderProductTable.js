e5e48d55085d36b67b69137c5d3eccc5f74ed583
montekaka <josh.chen.jia@gmail.com>
2018-12-03 20:25:31 -0800

    add Product name and product code

--- resource/orderProductTable.js
+++ resource/orderProductTable.js
@@ -1,9 +1,11 @@
 const skState = {
 	tableHeaders: [
-		{'id': 1,'name': "external_code", "format": "string", "label": "Product code", "sort_by":"ASC", "sort_on": true},
-		{'id': 2,'name': "external_name", "format": "string", "label":"Name", "sort_by":"ASC", "sort_on": true},
-		{'id': 3,'name': "external_price", "format": "currency", "label":"Price", "sort_by":"ASC", "sort_on": true},
-		{'id': 4,'name': "Select", "format": "select", "label":"","action":"GET"}
+		{'id': 1,'name': "product_code", "format": "string", "label": "Product code", "sort_by":"ASC", "sort_on": true},
+		{'id': 2,'name': "name", "format": "string", "label": "Product name", "sort_by":"ASC", "sort_on": true},
+		{'id': 3,'name': "external_code", "format": "string", "label": "External code", "sort_by":"ASC", "sort_on": true},
+		{'id': 4,'name': "external_name", "format": "string", "label":"External name", "sort_by":"ASC", "sort_on": true},
+		{'id': 5,'name': "external_price", "format": "currency", "label":"Price", "sort_by":"ASC", "sort_on": true},
+		{'id': 6,'name': "Select", "format": "select", "label":"","action":"GET"}
 	], 
 	params: {
 		SEARCH_TERM: '',
