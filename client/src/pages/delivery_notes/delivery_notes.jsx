237d3a022660c25689de745235cd24942ded8d59
montekaka <josh.chen.jia@gmail.com>
2018-12-06 22:10:27 -0800

    add delivery_notes component

---
+++ client/src/pages/delivery_notes/delivery_notes.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+import _ from 'underscore';
+import Dashheader from '../../components/dashheader/dashheader.jsx';
+import TablePagination from '../../components/shared/table-pagination/table-pagination.jsx';
+import config from '../../../../resource/config';
+
+//const tableHeaders = productTable.tableHeaders;
+const base_url = config.base_url;
+
+// parse page number from url
+
+class DeliveryNotes extends React.Component {
+  constructor(props) {    
+    super(props);
+    this.state = {
+      perPage: 8,     
+      pageItemsCount: 10,
+      totalPage: 0,
+      total: 0      
+    }
+    this.handleUpdateTotalItems = this.handleUpdateTotalItems.bind(this);
+    this.handleClickItem = this.handleClickItem.bind(this);
+  }
+  //getPaginationList = (currentPage, start, items, min, max)
+  
+  handleUpdateTotalItems(totalPage, total) {
+    this.setState({totalPage: totalPage, total: total});
+  }
+
+  handleClickItem(id) {
+    console.log(id)
+  }
+
+  render() {
+    if (this.props.isAuthed === false) {
+      return <Redirect to={'/'} />
+    }   
+    return (
+      <div>
+        <Dashheader subtitle={'Overview'} title={'Delivery notes'}/>
+        <div className="hr-divider mt-3 mb-5">
+          <h3 className="hr-divider-content hr-divider-heading">Buyer Companies</h3>          
+          <p>Total Delivery notes: {this.state.total}</p>
+          <p>Total Page: {this.state.totalPage}</p>
+        </div>        
+        <TablePagination     
+          handleUpdateTotalItems={this.handleUpdateTotalItems}    
+          base_url={base_url}
+          handleClickItem={this.handleClickItem} 
+          perPage={this.state.perPage}
+          pageItemsCount={this.state.pageItemsCount}
+          skState={this.props.skState}/>          
+      </div>      
+    )
+  }
+}
+
+export default DeliveryNotes;
