34accf5a82841c4a740b7e23e03638d1edabe548
montekaka <josh.chen.jia@gmail.com>
2018-12-16 18:07:58 -0800

    fix the pagination error

--- resource/libs/helpers/paginationList.js
+++ resource/libs/helpers/paginationList.js
@@ -1,18 +1,23 @@
 const _ = require('underscore');
 
 const getPaginationList = (currentPage, start, pageItemsCount, min, max) => {
-	let _pageItemsCount = pageItemsCount < max ? pageItemsCount : max;
 
-	let end = start + _pageItemsCount;
+	let incremental = pageItemsCount / 2;
+	let _pageItemsCount = max < pageItemsCount ? max : pageItemsCount;
 
-	let mid = Math.ceil(_pageItemsCount / 2) + start;
-	let change = currentPage > mid ? 1 : -1;
-	var newStart = start + change;
-	var newEnd = end + change;
-	if (newStart <= min || newEnd >= max) {
-		newStart = start;
-		newEnd = end
+	if (currentPage === start || currentPage === (start + pageItemsCount - 1)) {
+		newEnd = currentPage + incremental;
+		newStart = newEnd - _pageItemsCount;
+	} 
+	if (newEnd > max) {
+		newEnd = max + 1;
+		newStart = newEnd - _pageItemsCount;
+	} 
+	if (newStart < min) {
+		newStart = min;
+		newEnd = newStart + _pageItemsCount;
 	}
+
 	return _.range(newStart, newEnd);
 }
 
